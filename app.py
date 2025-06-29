6623ee86096ee62aa186df36ec7b029ebed91c27
miquelpairo <150731670+miquelpairo@users.noreply.github.com>
2025-06-29 19:45:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -408,10 +408,10 @@ def get_chart_range(param):
     """Define rangos personalizados para cada parámetro en los gráficos"""
     ranges = {
         'pH': [6, 8.5],
-        'Sal': [2000, 4700],
-        'Conductividad': [2000, 8300],
-        'TDS': [1000, 4700],
-        'ORP': [300, 800],
+        'Sal': [2000, 5000],
+        'Conductividad': [2000, 9000],
+        'TDS': [1000, 5000],
+        'ORP': [200, 800],
         'FAC': [0, 3],
         'Temperatura': [20, 35]
     }
