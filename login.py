be9b0d6d2af4b907a93d1d48b143ff6956f183ac
miquelpairo <150731670+miquelpairo@users.noreply.github.com>
2025-06-30 20:57:40 +0200

    Update login.py
    v2

--- login.py
+++ login.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
 def show_login_screen():
-    st.title("🔐 Bienvenido a Control de Piscinas")
-    st.markdown("Inicia sesión para acceder a tu panel personalizado.")
+    st.title("🔐 Bienvenido a Control de Piscina")
+    st.markdown("""
+        <div style="text-align: center; margin-top: 3rem;">
+            <h1 style="color: #2c3e50;">🔐 Acceso a Control Piscina</h1>
+            <p style="color: #7f8c8d;">Inicia sesión con tu cuenta de Google o crea un nuevo usuario</p>
+        </div>
+    """, unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
     with col1:
@@ -14,4 +19,3 @@ def show_login_screen():
         if st.button("➕ Crear nueva cuenta"):
             st.session_state["show_signup_form"] = True
             st.stop()
-
